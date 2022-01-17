2568f461fce383dea0c5067c8f2033778553e671
Amogus124 <95672691+Amogus124@users.noreply.github.com>
2022-01-17 14:10:42 -0400

    update CreditsState.hx

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -48,6 +48,10 @@ class CreditsState extends MusicBeatState
 		var pisspoop = [ //Name - Icon name - Description - Link - BG Color
 			['Psych Engine Android Port'],
 			['M.A. Jigsaw',		    'majigsaw',		    'Main Coder of The Port',	 'https://www.youtube.com/channel/UC2Sk7vtPzOvbVzdVTWrribQ',	'0xFFC30085'],
+			['Porter of Corruption'],
+			['PietroDev',		    'pietronicolas',		    'Main Coder',	 'https://youtube.com/c/PietronicolasOFC'],
+			['Helper'],
+			['Dxgamer',		    'dxgamer',		    'Helper',	 'https://youtube.com/c/Dxgamer7405'],
 			['Psych Engine Team'],
 			['Shadow Mario',		'shadowmario',		'Main Programmer of Psych Engine',					'https://twitter.com/Shadow_Mario_',	'0xFFFFDD33'],
 			['RiverOaken',			'riveroaken',		'Main Artist/Animator of Psych Engine',				'https://twitter.com/river_oaken',		'0xFFC30085'],
