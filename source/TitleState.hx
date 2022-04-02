de05d2d4baae11463b4263b61229f889a3414caf
Pietrodjaowjao <nicoleobenetti@gmail.com>
2022-04-02 14:18:23 -0300

    i forgorrrrr

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -65,9 +65,6 @@ class TitleState extends MusicBeatState
 		#if android
 		FlxG.android.preventDefaultKeys = [BACK];
 		#end
-
-		// Just to load a mod on start up if ya got one. For mods that change the menu music and bg
-		WeekData.loadTheFirstEnabledMod();
 		
 		//trace(path, FileSystem.exists(path));
 
